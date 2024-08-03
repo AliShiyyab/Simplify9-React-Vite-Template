1dbd0b8e4ba32f805a14c04003c4104bcf352f9c
abushiyyab <alis@simplify9.com>
2024-08-03 19:27:56 +0300

    Add Changes

--- src/create-simplify9-vite-app.ts
+++ src/create-simplify9-vite-app.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import * as fs from 'fs-extra';
+import { fileURLToPath } from 'url';
 import * as path from 'path';
 
 const projectName = process.argv[2];
@@ -9,6 +10,9 @@ if (!projectName) {
   console.log('  npx create-simplify9-vite-app my-app');
   process.exit(1);
 }
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const targetPath = path.join(process.cwd(), projectName);
 const templatePath = path.join(__dirname, 'template');
