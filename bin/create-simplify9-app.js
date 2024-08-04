ad074e90de61da01b1e93bd0a99fb4ed4be793f4
abushiyyab <alis@simplify9.com>
2024-08-04 13:19:04 +0300

    Change on file

--- bin/create-simplify9-app.js
+++ bin/create-simplify9-app.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
-import * as fs from 'fs';
-import * as path from 'path';
-import inquirer from 'inquirer';
+import { execSync } from 'child_process'
+import * as fs from 'fs'
+import * as path from 'path'
+import inquirer from 'inquirer'
 
 const runCommand = (command) => {
 	try {
-		execSync(command, { stdio: 'inherit' });
+		execSync(command, { stdio: 'inherit' })
 	} catch (error) {
-		console.error(`Error executing command: ${command}`, error);
-		process.exit(1);
+		console.error(`Error executing command: ${command}`, error)
+		process.exit(1)
 	}
-};
+}
 
 const createProject = async () => {
 	const { projectType, styling, useRedux, faviconPath, logoPath } = await inquirer.prompt([
@@ -43,66 +43,64 @@ const createProject = async () => {
 			name: 'logoPath',
 			message: 'Enter the path to your logo file:',
 		}
-	]);
+	])
 	
-	const projectName = '.';
+	const projectName = '.'
 	switch (projectType) {
 		case 'React Vite App JS':
-			runCommand(`npm create vite@latest ${projectName} -- --template react`);
-			break;
+			runCommand(`npm create vite@latest ${projectName} -- --template react`)
+			break
 		case 'React Vite App TS':
-			runCommand(`npm create vite@latest ${projectName} -- --template react-ts`);
-			break;
+			runCommand(`npm create vite@latest ${projectName} -- --template react-ts`)
+			break
 		case 'Next App JS':
-			runCommand(`npx create-next-app ${projectName}`);
-			break;
+			runCommand(`npx create-next-app ${projectName}`)
+			break
 		case 'Next App TS':
-			runCommand(`npx create-next-app ${projectName} --ts`);
-			break;
+			runCommand(`npx create-next-app ${projectName} --ts`)
+			break
 		default:
-			console.error('Unknown project type');
-			process.exit(1);
+			console.error('Unknown project type')
+			process.exit(1)
 	}
 	
-	process.chdir(projectName);
+	process.chdir(projectName)
 	
 	if (useRedux) {
-		runCommand('npm install @reduxjs/toolkit react-redux');
-		const fileType = projectType.includes('TS') ? 'ts' : 'js';
-		const baseDir = path.join('src', 'application');
-		const reduxDirs = ['api', 'reducers', 'store'];
-		reduxDirs.forEach(dir => fs.mkdirSync(path.join(baseDir, dir), { recursive: true }));
-		const reducerPath = path.join(baseDir, 'reducers', `index.${fileType}`);
-		fs.writeFileSync(reducerPath, `import { combineReducers } from 'redux';\n\nexport default combineReducers({});\n`);
-		const storePath = path.join(baseDir, 'store', `store.${fileType}`);
-		fs.writeFileSync(storePath, `import { configureStore } from '@reduxjs/toolkit';\nimport rootReducer from '../reducers';\n\nconst store = configureStore({ reducer: rootReducer });\n\nexport default store;\n`);
+		runCommand('npm install @reduxjs/toolkit react-redux')
+		const fileType = projectType.includes('TS') ? 'ts' : 'js'
+		const baseDir = path.join('src', 'application')
+		const reduxDirs = ['api', 'reducers', 'store']
+		reduxDirs.forEach(dir => fs.mkdirSync(path.join(baseDir, dir), { recursive: true }))
+		const storePath = path.join(baseDir, 'store', `store.${fileType}`)
+		fs.writeFileSync(storePath, `import {combineReducers, configureStore} from "@reduxjs/toolkit";\nimport {FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE} from "redux-persist";\n\nconst reducers = combineReducers({});\nconst persistConfig = { ket: "root", whitelist: [], storage };\nconst persistedReducer = persistReducer(persistConfig, reducers);\nconst store = configureStore({ reducer: persistedReducer, middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: { ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER] }}).concat() });\n\nexport default store;\n`)
 	}
 	
-	const componentsDir = path.join('src', 'components');
-	fs.mkdirSync(componentsDir, { recursive: true });
-	const sharesDir = path.join(componentsDir, 'shares');
-	fs.mkdirSync(sharesDir, { recursive: true });
+	const componentsDir = path.join('src', 'components')
+	fs.mkdirSync(componentsDir, { recursive: true })
+	const sharesDir = path.join(componentsDir, 'shares')
+	fs.mkdirSync(sharesDir, { recursive: true })
 	const componentFileType = projectType.includes('TS') ? 'tsx' : 'js';
 	['niceSelect', 'niceButton', 'niceInputField', 'niceText'].forEach(file => {
-		const filePath = path.join(sharesDir, `${file}.${componentFileType}`);
-		const fileContent = `\n\nconst ${file} = () => (\n  <div className="${styling === 'Material UI' ? '' : 'tailwind-class'}">\n    {/* Component code here */}\n  </div>\n);\n\nexport default ${file};\n`;
-		fs.writeFileSync(filePath, fileContent);
-	});
+		const filePath = path.join(sharesDir, `${file}.${componentFileType}`)
+		const fileContent = `\n\nconst ${file} = () => (\n  <div className="${styling === 'Material UI' ? '' : 'tailwind-class'}">\n    {/* Component code here */}\n  </div>\n);\n\nexport default ${file};\n`
+		fs.writeFileSync(filePath, fileContent)
+	})
 	
-	if (faviconPath) fs.copyFileSync(faviconPath, path.join('public', 'favicon.ico'));
-	if (logoPath) fs.copyFileSync(logoPath, path.join('public', 'logo.png'));
+	if (faviconPath) fs.copyFileSync(faviconPath, path.join('public', 'favicon.ico'))
+	if (logoPath) fs.copyFileSync(logoPath, path.join('public', 'logo.png'))
 	
 	if (styling === 'Material UI') {
-		runCommand('npm install @mui/material @emotion/react @emotion/styled');
-		const themeProviderContent = `import { createTheme, ThemeProvider } from '@mui/material/styles';\nimport CssBaseline from '@mui/material/CssBaseline';\n\nconst theme = createTheme();\n\nconst ThemeProviderWrapper = ({ children }: any) => (\n  <ThemeProvider theme={theme}>\n    <CssBaseline />\n    {children}\n  </ThemeProvider>\n);\n\nexport default ThemeProviderWrapper;\n`;
+		runCommand('npm install @mui/material @emotion/react @emotion/styled')
+		const themeProviderContent = `import { createTheme, ThemeProvider } from '@mui/material/styles';\nimport CssBaseline from '@mui/material/CssBaseline';\n\nconst theme = createTheme();\n\nconst ThemeProviderWrapper = ({ children }: any) => (\n  <ThemeProvider theme={theme}>\n    <CssBaseline />\n    {children}\n  </ThemeProvider>\n);\n\nexport default ThemeProviderWrapper;\n`
 		
-		const appFilePath = path.join('src', `App.${componentFileType}`);
-		const appContent = fs.readFileSync(appFilePath, 'utf8');
-		fs.writeFileSync(appFilePath, `${appContent.replace('<React.StrictMode>', '<React.StrictMode><ThemeProviderWrapper>').replace('</React.StrictMode>', '</ThemeProviderWrapper></React.StrictMode>')}`);
-		fs.writeFileSync(path.join('src', `ThemeProviderWrapper.${componentFileType}`), themeProviderContent);
+		const appFilePath = path.join('src', `App.${componentFileType}`)
+		const appContent = fs.readFileSync(appFilePath, 'utf8')
+		fs.writeFileSync(appFilePath, `${appContent.replace('<React.StrictMode>', '<React.StrictMode><ThemeProviderWrapper>').replace('</React.StrictMode>', '</ThemeProviderWrapper></React.StrictMode>')}`)
+		fs.writeFileSync(path.join('src', `ThemeProviderWrapper.${componentFileType}`), themeProviderContent)
 	}
 	
-	console.log('Project setup complete!');
-};
+	console.log('Project setup complete!')
+}
 
-createProject();
+createProject()
